30c31065f2f5034bc42e07938b7476e5d4fce40d
UnholyTsar <56408528+MABatin@users.noreply.github.com>
2022-11-28 10:11:48 +0600

    Update balance.py

--- balance.py
+++ balance.py
@@ -12,8 +12,6 @@ def balance(dataset):
     oversampling = RandomOverSampler()
 
     X, y = oversampling.fit_resample(X, y)
-    # balanced_dataset = X[0].to_frame().join(y.to_frame())
-    # balanced_dataset = balanced_dataset.join(X.iloc[:, 1:])
     balanced_dataset = X.join(y.to_frame())
 
     return balanced_dataset
@@ -23,7 +21,7 @@ def main():
     in_csv = 'results/unbalanced_keypoints.csv'
     out_csv = 'results/balanced_keypoints.csv'
 
-    dataset = pd.read_csv(in_csv, header=None, delimiter=',')
+    dataset = pd.read_csv(in_csv, header=None, sep=',')
 
     balanced_dataset = balance(dataset)
 
